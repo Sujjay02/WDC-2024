e58eb9b66138e12314272f992b51804b5c173185
Sujjay02 <63737819+Sujjay02@users.noreply.github.com>
2024-04-11 22:58:22 -0400

    Changed float to int, no graphing function yet

--- voltage.ino
+++ voltage.ino
@@ -6,13 +6,14 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin 1:
-  int sensorValue = analogRead(A1);
+  int sensorValue = analogRead(A0);
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
+  int voltage = sensorValue * (5.0 / 1023.0);
+
   // print out the value you read:
-  if(sensorValue >= 0){
-      Serial.println("Voltage: "+voltage);
-      Serial.println("Sensor Values: "+sensorVal); //checks to make sure that sensor is returning data
+  if(sensorValue >= 0){ //checks to make sure that sensor is returning data
+      Serial.println("Voltage Values: "+voltage); 
+      Serial.println("Sensor Values: "+sensorValue); 
   }  
   else{
     Serial.println("Sensor is not positioned correctly/needs changing");
