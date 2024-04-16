a1b0977b6923ecfe965cd44c00c7504e31c5e34a
Sujjay02 <63737819+Sujjay02@users.noreply.github.com>
2024-04-15 22:25:55 -0400

    Final Comp Changes

--- voltage.ino
+++ voltage.ino
@@ -1,6 +1,3 @@
-
-
-// the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
@@ -9,9 +6,9 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin 0:
-  double sensorValue = analogRead(A0);
-  double finalVal = sensorValue * (5.0 / 1023.0);
+  int sensorValue = analogRead(A0);
+  int finalVal = sensorValue * (5.0 / 1023.0);
   // print out the value you read:
-  Serial.println(finalVal);
-  delay(1000);  // delay in between reads for stability
+  Serial.println("Voltage:"+finalVal);
+  delay(100);  // delay in between reads for stability
 }
