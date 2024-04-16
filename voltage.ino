9bc7aaf2ef14403ecd06eb67a9bb074c206008e7
Sujjay02 <63737819+Sujjay02@users.noreply.github.com>
2024-04-15 21:34:43 -0400

    Update voltage.ino

--- voltage.ino
+++ voltage.ino
@@ -1,3 +1,6 @@
+
+
+// the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
@@ -5,18 +8,10 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 1:
-  int sensorValue = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  int voltage = sensorValue * (5.0 / 1023.0);
-
+  // read the input on analog pin 0:
+  double sensorValue = analogRead(A0);
+  double finalVal = sensorValue * (5.0 / 1023.0);
   // print out the value you read:
-  if(sensorValue >= 0){ //checks to make sure that sensor is returning data
-      Serial.println("Voltage Values: "+voltage); 
-      Serial.println("Sensor Values: "+sensorValue); 
-  }  
-  else{
-    Serial.println("Sensor is not positioned correctly/needs changing");
-  }
- 
+  Serial.println(finalVal);
+  delay(1000);  // delay in between reads for stability
 }
